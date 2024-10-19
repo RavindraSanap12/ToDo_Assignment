64a6e20d5ef728a792ffb1619816bc8691813bf6
RavindraSanap12 <ravindrasanap978@gmail.com>
2024-10-19 22:26:44 +0530

    first commit

--- src/main/java/com/todo/ToDoApp/controller/TodoController.java
+++ src/main/java/com/todo/ToDoApp/controller/TodoController.java
@@ -3,13 +3,14 @@ package com.todo.ToDoApp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.todo.ToDoApp.Entity.Todo;
 import com.todo.ToDoApp.service.TodoService;
-// jiijihiuhiuh
-giuguig
 
-oi8kuk
 @Controller
 public class TodoController {
 
